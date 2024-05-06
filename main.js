c6b884382f64d1322a1e30398e79d6a8395fc51d
Salomão <salomaocabral00@gmail.com>
2024-05-06 14:23:03 -0300

    msg

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento === null) {
-        console.log('Elemento não encontrado');
-    }
-
-    if (elemento != null)
-
-        if (elemento.localName === 'audio') {
+    if (elemento && elemento.localName === 'audio'){
             elemento.play();
+        } else {
+            console.log('Elemento não encontrado ou seletor inválido');
         }
-       
  }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
